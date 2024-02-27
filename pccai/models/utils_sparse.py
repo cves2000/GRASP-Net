a0c074a69d6646eeacdc4b98bd8717260bd58ade
cves2000 <130291630+cves2000@users.noreply.github.com>
2024-02-27 20:24:01 +0800

    Update utils_sparse.py

--- pccai/models/utils_sparse.py
+++ pccai/models/utils_sparse.py
@@ -6,26 +6,32 @@
 
 # Utility functions for sparse tensors
 
+# 导入所需的库
 import torch
 import numpy as np
 import MinkowskiEngine as ME
 
-
+# 定义一个函数，用于对稀疏张量进行缩放
 def scale_sparse_tensor_batch(x, factor):
+    # 将x的坐标乘以缩放因子并四舍五入为整数
     coords = torch.hstack((x.C[:,0:1], (x.C[:,1:]*factor).round().int()))
+    # 创建一个新的稀疏张量，其中的特征是全1，坐标是缩放后的坐标
     feats = torch.ones((len(coords),1)).float()
     x = ME.SparseTensor(features=feats, coordinates=coords, tensor_stride=1, device=x.device)
     return x
 
-
+# 定义一个函数，用于根据坐标对稀疏张量进行排序
 def sort_sparse_tensor_with_dir(sparse_tensor, dir=1):
     """
     Sort points in sparse tensor according to their coordinates.
     """
+    # 计算一个向量，该向量是sparse_tensor的坐标的加权和，其中权重是坐标的最大值加1的幂
     vec = sum([sparse_tensor.C.long().cpu()[:, i] * 
         (sparse_tensor.C.cpu().max().long() + 1) ** (i if dir==0 else (3 - i)) 
         for i in range(sparse_tensor.C.shape[-1])])
+    # 根据这个向量对sparse_tensor的特征和坐标进行排序
     indices_sort = np.argsort(vec)
+    # 返回一个新的稀疏张量，其中的特征和坐标是排序后的特征和坐标
     sparse_tensor_sort = ME.SparseTensor(features=sparse_tensor.F[indices_sort], 
                                          coordinates=sparse_tensor.C[indices_sort],
                                          tensor_stride=sparse_tensor.tensor_stride[0], 
@@ -33,8 +39,12 @@ def sort_sparse_tensor_with_dir(sparse_tensor, dir=1):
     return sparse_tensor_sort
 
 
+
 def slice_sparse_tensor(x, slice):
     '''
+    slice_sparse_tensor函数用于对稀疏张量进行切片，最多可以获取8个切片。它接受一个稀疏张量x和一个切片数slice。
+    如果slice为0，那么直接返回x。否则，它首先计算x的坐标的方差和中位数，然后根据方差对坐标进行排序。
+    接着，根据slice的值，生成一系列的掩码，然后使用这些掩码对x进行切片，最后返回切片后的稀疏张量列表。
     A simple function to slice a sparse tensor, can at most get 8 slices
     '''
 
@@ -91,3 +101,5 @@ class AxisSlice:
 
     def nm(self):
         return torch.logical_not(self.mask)
+    # AxisSlice类用于存储坐标的信息。它接受一个名称name，一个方差var，一个阈值thres和一个掩码mask，
+    # 然后将这些信息存储为类的属性。
