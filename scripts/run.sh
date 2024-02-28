372bb934467e96108506360924c161a846e44e36
cves2000 <130291630+cves2000@users.noreply.github.com>
2024-02-28 10:08:17 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -31,3 +31,8 @@ fi
 # 如果LAUNCHER的值是"f"，那么就在前台直接运行这个脚本。
 # 如果LAUNCHER的值是"s"，那么就使用Slurm作业调度器来运行这个脚本。
 # 如果LAUNCHER的值是其他的值，那么就打印出"No launcher is specified."。
+# 这个Bash脚本的主要用途是在特定的CUDA设备上运行Python脚本，并且可以选择在后台、前台或使用Slurm作业调度器来运行。具体来说，它做了以下几件事：
+# 解析输入参数：脚本接受三个参数。第一个参数SPEC是你想要运行的Python脚本的名称，第二个参数LAUNCHER决定了如何运行Python脚本（直接在后台运行、直接在前台运行或使用Slurm作业调度器运行），第三个参数USE_GPU是你想要使用的CUDA设备的编号。
+# 生成运行参数：脚本运行./utils/gen_args.py Python脚本，并将其输出作为运行Python脚本的参数。
+# 运行Python脚本：根据LAUNCHER变量的值来决定如何运行另一个脚本./scripts/config_args.sh。这个脚本负责设置环境变量并运行Python脚本。
+# 总的来说，这个脚本的作用是让你能够方便地在特定的CUDA设备上运行Python脚本，并向这个脚本传递参数。
