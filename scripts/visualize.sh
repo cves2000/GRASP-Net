c1c4b02001d764f26d20ecf8dbbf9fc02e3753cf
cves2000 <130291630+cves2000@users.noreply.github.com>
2024-02-28 10:18:44 +0800

    Update visualize.sh

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env bash 该脚本可能用于可视化3D数据
+ 
 # Copyright (c) 2010-2022, InterDigital
 # All rights reserved. 
 
@@ -20,3 +20,5 @@ python ./utils/visualize.py \
 --radius $RADIUS \
 --radius_origin $RADIUS_ORIGIN \
 --window_name $FILE
+# 这部分代码运行了visualize.py脚本，并将之前定义的变量作为参数传递给这个脚本。
+# 这个脚本的功能可能是读取3D数据文件，并根据给定的参数生成一个可视化的窗口。
