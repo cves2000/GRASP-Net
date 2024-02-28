a3806694e8dbc0392d61ba4fd1a7b81c1605ca54
cves2000 <130291630+cves2000@users.noreply.github.com>
2024-02-28 15:06:18 +0800

    Update option_handler.py

--- pccai/utils/option_handler.py
+++ pccai/utils/option_handler.py
@@ -1,137 +1,139 @@
-# Copyright (c) 2010-2022, InterDigital
-# All rights reserved. 
+# 版权所有 (c) 2010-2022，InterDigital
+# 保留所有权利。
+# 请在根文件夹下查看LICENSE。
 
-# See LICENSE under the root folder.
+# 在训练、测试、基准测试等过程中处理所有输入参数。
 
+import pccai.utils.logger as logger  # 导入pccai.utils.logger作为logger
+import argparse  # 导入argparse库
+import os  # 导入os库
 
-# Handle all the input argumants during training, testing, benchmarking, etc.
-
-import pccai.utils.logger as logger
-import argparse
-import os
-
-def str2bool(val):
-    if isinstance(val, bool):
-        return val
-    if val.lower() in ('true', 'yes', 't', 'y', '1'):
-        return True
-    elif val.lower() in ('false', 'no', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Expect a Boolean value.')
+def str2bool(val):  # 定义str2bool函数
+    if isinstance(val, bool):  # 如果val是布尔值
+        return val  # 返回val
+    if val.lower() in ('true', 'yes', 't', 'y', '1'):  # 如果val在这些值中
+        return True  # 返回True
+    elif val.lower() in ('false', 'no', 'f', 'n', '0'):  # 如果val在这些值中
+        return False  # 返回False
+    else:  # 否则
+        raise argparse.ArgumentTypeError('Expect a Boolean value.')  # 抛出异常
 
 
-class BasicOptionHandler():
-    """A class that includes the basic options sharing among all phases."""
+class BasicOptionHandler():  # 定义BasicOptionHandler类
+    """包含所有阶段共享的基本选项的类。"""
 
-    def __init__(self):
-        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        parser = self.add_options(parser)
-        self.parser = parser
+    def __init__(self):  # 初始化函数
+        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # 创建解析器
+        parser = self.add_options(parser)  # 添加选项
+        self.parser = parser  # 设置解析器
 
-    def add_options(self, parser):
+    def add_options(self, parser):  # 定义添加选项的函数
 
-        # What do you want to do
-        parser.add_argument('--exp_name', type=str, default='experiment_name', help='Name of the experiment, result folder is created based on this name.')
+        # 你想做什么
+        parser.add_argument('--exp_name', type=str, default='experiment_name', help='实验的名称，根据此名称创建结果文件夹。')
 
-        # What are your ingredients
-        parser.add_argument('--net_config', type=str, default='', help='Network configuration in YAML.')
-        parser.add_argument('--optim_config', type=str, default='', help='Optimization configuration in YAML.')
-        parser.add_argument('--hetero', type=str2bool, nargs='?', const=True, default=False, help='Whether to use the heterogeneous batching mode.')
-        parser.add_argument('--checkpoint', type=str, default='', help='Load an existing checkpoint.')
+        # 你的原料是什么
+        parser.add_argument('--net_config', type=str, default='', help='YAML中的网络配置。')
+        parser.add_argument('--optim_config', type=str, default='', help='YAML中的优化配置。')
+        parser.add_argument('--hetero', type=str2bool, nargs='?', const=True, default=False, help='是否使用异构批处理模式。')
+        parser.add_argument('--checkpoint', type=str, default='', help='加载现有的检查点。')
 
-        # How do you cook
-        parser.add_argument('--alpha', type=float, default=None, help='Weight for distortion in R-D optimization, can overwrite the one in the YAML config.')
-        parser.add_argument('--beta', type=float, default=None, help='Weight for bit-rate in R-D optimization, can overwrite the one in the YAML config.')
-        parser.add_argument('--seed', type=float, default=None, help='Set random seed for reproducibility')
+        # 你怎么做
+        parser.add_argument('--alpha', type=float, default=None, help='R-D优化中的失真权重，可以覆盖YAML配置中的值。')
+        parser.add_argument('--beta', type=float, default=None, help='R-D优化中的比特率权重，可以覆盖YAML配置中的值。')
+        parser.add_argument('--seed', type=float, default=None, help='设置随机种子以实现可重复性')
 
-        # Logging options
-        parser.add_argument('--result_folder', type=str, default='results', help='Indicate the result folder.')
-        parser.add_argument('--log_file', type=str, default='', help='Log file name.')
-        parser.add_argument('--log_file_only', type=str2bool, nargs='?', const=True, default=False, help='Only prints to the log file if set True.')
-        parser.add_argument('--print_freq', type=int, default=20, help='Frequency of displaying results.')
-        parser.add_argument('--pc_write_freq', type=int, default=50, help='Frequency of writing down the point cloud, use tensorboard to write during training, write "ply" file proint cloud during testing.')  
-        parser.add_argument('--tf_summary', type=str2bool, nargs='?', const=True, default=False, help='Whether to use tensorboard for log.')
-        return parser
+        # 记录选项
+        parser.add_argument('--result_folder', type=str, default='results', help='指定结果文件夹。')
+        parser.add_argument('--log_file', type=str, default='', help='日志文件名。')
+        parser.add_argument('--log_file_only', type=str2bool, nargs='?', const=True, default=False, help='如果设置为True，只打印到日志文件。')
+        parser.add_argument('--print_freq', type=int, default=20, help='显示结果的频率。')
+        parser.add_argument('--pc_write_freq', type=int, default=50, help='写下点云的频率，使用tensorboard在训练期间写入，测试期间写入"ply"文件proint cloud。')  
+        parser.add_argument('--tf_summary', type=str2bool, nargs='?', const=True, default=False, help='是否使用tensorboard进行日志。')
+        return parser  # 返回解析器
 
-    def parse_options(self):
-        opt, _ = self.parser.parse_known_args()
-        opt.exp_folder = os.path.join(opt.result_folder, opt.exp_name)
-        return opt
+    def parse_options(self):  # 定义解析选项的函数
+        opt, _ = self.parser.parse_known_args()  # 解析已知参数
+        opt.exp_folder = os.path.join(opt.result_folder, opt.exp_name)  # 设置实验文件夹
+        return opt  # 返回选项
 
-    def print_options(self, opt):
-        message = ''
-        message += '\n----------------- Input Arguments ---------------\n'
+    def print_options(self, opt):  # 定义打印选项的函数
+        message = ''  # 初始化消息为空
+        message += '\\n----------------- 输入参数 ---------------\\n'
         # For k, v in sorted(vars(opt).items()):
-        for k, v in vars(opt).items():
-            comment = ''
+        for k, v in vars(opt).items():  # 对于选项中的每一项
+            comment = ''  # 初始化注释为空
             default = self.parser.get_default(k)
             if v != default:
                 comment = '\t[default: %s]' % str(default)
             message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
         message += '----------------- End -------------------'
         logger.log.info(message)
+# 这段代码主要用于在训练、测试、基准测试等过程中处理所有输入参数。
+# 其中，str2bool函数用于将字符串转换为布尔值，BasicOptionHandler类用于处理所有阶段共享的基本选项，包括添加选项、解析选项和打印选项。
 
 
-class TrainOptionHandler(BasicOptionHandler):
-    """A class that includes specific options for training."""
 
-    def add_options(self, parser):
-        parser = BasicOptionHandler.add_options(self, parser)
-        parser.add_argument('--train_data_config', type=str, nargs='+', required=True, help='Training data configuration in YAML.')
-        parser.add_argument('--val_data_config', type=str, nargs='+', default='', help='Validataion data configuration in YAML.')
-        parser.add_argument('--checkpoint_optim_config', type=str2bool, nargs='?', const=True, default=False, help='Whether to load the optimizers and schedulers from checkpoint.')
-        parser.add_argument('--checkpoint_epoch_state', type=str2bool, nargs='?', const=True, default=False, help='Whether to load the epoch state from the checkpoint.')
-        parser.add_argument('--save_checkpoint_freq', type=int, default=2, help='Frequency of saving the trained model.')
-        parser.add_argument('--save_checkpoint_max', type=int, default=10, help='Maximum number of check points to be saved.')
-        parser.add_argument('--save_checkpoint_prefix', type=str, default='epoch_', help='Prefix of the check points file names. ')
-        parser.add_argument('--val_freq', type=int, default=-1, help='Frequency of validation with the validation set, <=0 means no validation.')
-        parser.add_argument('--val_print_freq', type=int, default=20, help='Frequency of displaying results during validation.')
-        parser.add_argument('--lr', type=float, default=None, help='Learning rate of the main parameters, can overwrite the one in the YAML config.')
-        parser.add_argument('--lr_aux', type=float, default=None, help='Learning rate of the aux parameters, can overwrite the one in the YAML config.')
-        parser.add_argument('--fix_modules', type=str, nargs='+', default='', help='Names of the fixed modules during training.')
-        parser.add_argument('--ddp', type=str2bool, nargs='?', const=True, default=False, help='Whether to DPP mode or not.')
-        parser.add_argument('--master_address', type=str, default='localhost', help='Master address of DDP.')
-        parser.add_argument('--master_port', type=int, default=29500, help='Master port of DPP.')
+class TrainOptionHandler(BasicOptionHandler):  # 定义TrainOptionHandler类，包含训练的特定选项
+    """包含训练的特定选项的类。"""
 
-        # Method-specific parameters
-        parser.add_argument('--scaling_ratio', type=float, required=True, help='[GRASP] Scaling ratio.')
-        parser.add_argument('--point_mul', type=int, required=True, help='[GRASP] Point multiplication parameter.')
-        parser.add_argument('--skip_mode',  type=str2bool, nargs='?', required=True, const=True, default=False, help='[GRASP] Skip mode or not.')
+    def add_options(self, parser):  # 定义添加选项的函数
+        parser = BasicOptionHandler.add_options(self, parser)  # 添加基本选项
+        parser.add_argument('--train_data_config', type=str, nargs='+', required=True, help='YAML中的训练数据配置。')
+        parser.add_argument('--val_data_config', type=str, nargs='+', default='', help='YAML中的验证数据配置。')
+        parser.add_argument('--checkpoint_optim_config', type=str2bool, nargs='?', const=True, default=False, help='是否从检查点加载优化器和调度器。')
+        parser.add_argument('--checkpoint_epoch_state', type=str2bool, nargs='?', const=True, default=False, help='是否从检查点加载epoch状态。')
+        parser.add_argument('--save_checkpoint_freq', type=int, default=2, help='保存训练模型的频率。')
+        parser.add_argument('--save_checkpoint_max', type=int, default=10, help='保存的检查点的最大数量。')
+        parser.add_argument('--save_checkpoint_prefix', type=str, default='epoch_', help='检查点文件名的前缀。')
+        parser.add_argument('--val_freq', type=int, default=-1, help='使用验证集进行验证的频率，<=0表示不进行验证。')
+        parser.add_argument('--val_print_freq', type=int, default=20, help='在验证期间显示结果的频率。')
+        parser.add_argument('--lr', type=float, default=None, help='主参数的学习率，可以覆盖YAML配置中的值。')
+        parser.add_argument('--lr_aux', type=float, default=None, help='辅助参数的学习率，可以覆盖YAML配置中的值。')
+        parser.add_argument('--fix_modules', type=str, nargs='+', default='', help='在训练期间固定的模块的名称。')
+        parser.add_argument('--ddp', type=str2bool, nargs='?', const=True, default=False, help='是否使用DPP模式。')
+        parser.add_argument('--master_address', type=str, default='localhost', help='DDP的主地址。')
+        parser.add_argument('--master_port', type=int, default=29500, help='DDP的主端口。')
 
-        # You can add your method-specific parameters here if necessary. They can be passed to the loaded YAML configs before training.
-        # Check how alpha, beta and lr are overwritten in pipelines/train.py as examples.
+        # 方法特定的参数
+        parser.add_argument('--scaling_ratio', type=float, required=True, help='[GRASP] 缩放比例。')
+        parser.add_argument('--point_mul', type=int, required=True, help='[GRASP] 点乘参数。')
+        parser.add_argument('--skip_mode',  type=str2bool, nargs='?', required=True, const=True, default=False, help='[GRASP] 是否跳过模式。')
+
+        # 如果需要，你可以在这里添加你的方法特定的参数。他们可以在训练前传递给加载的YAML配置。
+        # 查看pipelines/train.py中如何覆盖alpha、beta和lr作为示例。
         return parser
 
 
-class TestOptionHandler(BasicOptionHandler):
-    """A class that includes specific options for tesing."""
+class TestOptionHandler(BasicOptionHandler):  # 定义TestOptionHandler类，包含测试的特定选项
+    """包含测试的特定选项的类。"""
 
-    def add_options(self, parser):
-        parser = BasicOptionHandler.add_options(self, parser)
-        parser.add_argument('--checkpoint_net_config', type=str2bool, nargs='?', const=True, default=False, help='Whether to load the model configuration from the checkpoint, if yes, net_config will be ignored.')
-        parser.add_argument('--test_data_config', type=str, nargs='+', required=True, help='Test data configuration in YAML.')
-        parser.add_argument('--gen_bitstream', type=str2bool, nargs='?', const=True, default=False, help='Generate the actual bitstream or not.')
-        parser.add_argument('--pc_write_prefix', type=str, default='', help='Prefix when writing down the point clouds.')  
+    def add_options(self, parser):  # 定义添加选项的函数
+        parser = BasicOptionHandler.add_options(self, parser)  # 添加基本选项
+        parser.add_argument('--checkpoint_net_config', type=str2bool, nargs='?', const=True, default=False, help='是否从检查点加载模型配置，如果是，将忽略net_config。')
+        parser.add_argument('--test_data_config', type=str, nargs='+', required=True, help='YAML中的测试数据配置。')
+        parser.add_argument('--gen_bitstream', type=str2bool, nargs='?', const=True, default=False, help='是否生成实际的比特流。')
+        parser.add_argument('--pc_write_prefix', type=str, default='', help='写下点云时的前缀。')  
         return parser
+# 这段代码主要用于在训练和测试过程中处理所有输入参数。其中，TrainOptionHandler类用于处理训练的特定选项，TestOptionHandler类用于处理测试的特定选项。
 
+class BenchmarkOptionHandler(BasicOptionHandler):  # 定义BenchmarkOptionHandler类，包含基准测试的特定选项
+    """包含基准测试的特定选项的类。"""
 
-class BenchmarkOptionHandler(BasicOptionHandler):
-    """A class that includes specific options for benchmarking."""
-
-    def add_options(self, parser):
-        parser = BasicOptionHandler.add_options(self, parser)
-        parser.add_argument('--checkpoints', type=str, nargs='+', default=None, help='Specify several existing checkpoints.')
-        parser.add_argument('--checkpoint_net_config', type=str2bool, nargs='?', const=True, default=True, help='Whether to load the model configuration from the checkpoint, if yes, net_config will be ignored.')
-        parser.add_argument('--codec_config', type=str, required=True, help='Codec configuration in YAML.')
-        parser.add_argument('--input', type=str, nargs='+', required=True, help='A list of folders containing the point clouds to be tested, or simply just a ply file.')
-        parser.add_argument('--peak_value', type=int, nargs='+', required=True, help='Peak value(s) for computing the D1 and D2 metrics. If only one value is provided, it will be used for the whole test; otherwise peak values for every point clouds need to be given.')
-        parser.add_argument('--bit_depth', type=int, nargs='+', required=True, help='Bit-depth value(s) of the point cloud(s) to be tested. If only one value is provided, it will be used for the whole test; otherwise bit-depths for every point clouds need to be given.')
-        parser.add_argument('--remove_compressed_files', type=str2bool, nargs='?', const=True, default=True, help='Whether to remove the compressed files.')
-        parser.add_argument('--skip_decode', type=str2bool, nargs='?', const=True, default=False, help='Whether to skip the decoding process, useful for lossless compression.')
-        parser.add_argument('--compute_d2', type=str2bool, nargs='?', const=True, default=False, help='Whether to compute the D2 metric.')
-        parser.add_argument('--mpeg_report', type=str, default=None, help='Write the results for MPEG reporting in the CSV format.')
-        parser.add_argument('--mpeg_report_sequence', type=str2bool, nargs='?', const=True, default=False, help='If true, create MPEG report in the CSV format by viewing the inputs as point cloud sequences.')
-        parser.add_argument('--write_prefix', type=str, default='', help='Prefix when writing down the point clouds and the bitstreams.')
-        parser.add_argument('--slice', type=int, default=None, help='Slicing parameter.')
+    def add_options(self, parser):  # 定义添加选项的函数
+        parser = BasicOptionHandler.add_options(self, parser)  # 添加基本选项
+        parser.add_argument('--checkpoints', type=str, nargs='+', default=None, help='指定几个现有的检查点。')
+        parser.add_argument('--checkpoint_net_config', type=str2bool, nargs='?', const=True, default=True, help='是否从检查点加载模型配置，如果是，将忽略net_config。')
+        parser.add_argument('--codec_config', type=str, required=True, help='YAML中的编解码器配置。')
+        parser.add_argument('--input', type=str, nargs='+', required=True, help='包含要测试的点云的文件夹列表，或者只是一个ply文件。')
+        parser.add_argument('--peak_value', type=int, nargs='+', required=True, help='计算D1和D2度量的峰值。如果只提供了一个值，它将用于整个测试；否则需要为每个点云给出峰值。')
+        parser.add_argument('--bit_depth', type=int, nargs='+', required=True, help='要测试的点云的位深值。如果只提供了一个值，它将用于整个测试；否则需要为每个点云给出位深。')
+        parser.add_argument('--remove_compressed_files', type=str2bool, nargs='?', const=True, default=True, help='是否删除压缩文件。')
+        parser.add_argument('--skip_decode', type=str2bool, nargs='?', const=True, default=False, help='是否跳过解码过程，对于无损压缩很有用。')
+        parser.add_argument('--compute_d2', type=str2bool, nargs='?', const=True, default=False, help='是否计算D2度量。')
+        parser.add_argument('--mpeg_report', type=str, default=None, help='以CSV格式写入MPEG报告的结果。')
+        parser.add_argument('--mpeg_report_sequence', type=str2bool, nargs='?', const=True, default=False, help='如果为true，通过将输入视为点云序列，以CSV格式创建MPEG报告。')
+        parser.add_argument('--write_prefix', type=str, default='', help='写下点云和比特流时的前缀。')
+        parser.add_argument('--slice', type=int, default=None, help='切片参数。')
         return parser
+# 这段代码主要用于在基准测试过程中处理所有输入参数。其中，BenchmarkOptionHandler类用于处理基准测试的特定选项。
